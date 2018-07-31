443a1c2b4a828c33c992230fa4a54cff6fc5987c
Gaurav Dang <gadang@BLR.ALLEGISINDIA.COM>
2018-07-31 18:33:59 +0530

    Second Day

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,21 +1,37 @@
 package io.javabrains.springbootstarter.topic;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+//import java.lang.reflect.Method;
+//import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicController {
-    @RequestMapping("/Topics")
+    @Autowired
+    private TopicService topicService;
+
+    //
+    @RequestMapping("/topics")
     public List<Topic> getAllTopics()
     {
-        return Arrays.asList(
-                new Topic("spring","Spring Framework","Spring Framework Description"),
-                new Topic("Java","Core Java","Core Java Description"),
-                new Topic("javascript","Javascript","Javascript Description")
-        );
+
+        return topicService.getAllTopics();
     }
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic
+            (@PathVariable String id)
+    {
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value = "/topics")
+    public void addTopic(@RequestBody Topic topic)
+    {
+        topicService.addTopic(topic);
+    }
+
 }
